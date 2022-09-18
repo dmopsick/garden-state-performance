ed241607a17a5a11ec8fe59837bd71af21bc511c
Dan Mopsick <danmopsick@gmail.com>
2022-09-18 12:21:27 -0400

    Modifying the home page text as Shane sees fit. This is the way

--- src/components/Home.js
+++ src/components/Home.js
@@ -114,18 +114,14 @@ export default function Home() {
                             
                         <ArrowDownwardIcon className={classes.movingArrow}  style={{fontSize: "96px"}} />
                     </Link>
-
-                    <a className={classes.hoursLink} target="_blank" href="/resources/GSP-Summer-Hours-2022.pdf">
-                        Summer 2022 Hours
-                    </a>
                 
                 </div>
             </div>
         
         
             <div id="info" className={classes.section}>
-                <h1 className={classes.sectionHeader}>Hustle. Grit. Grind.</h1> 
-                <p className={classes.sectionCaption}>Any athlete. Any goal. You've just got to do the <em>work</em></p>
+                <h1 className={classes.sectionHeader}>Movement. Strength. Performance.</h1> 
+                <p className={classes.sectionCaption}>Become a better human - a better athlete - a better player</p>
                     
                     <div style={{ 
                         display: "flex",
@@ -133,11 +129,11 @@ export default function Home() {
                     }}>
 
                         <div className={classes.bodyText}>
-                            <h1>Athlete Training</h1>
+                            <h1>Training at GSP</h1>
                             <ul className={classes.infoList}>
+                            <li className={classes.infoListItem}>Learn the proper movement patterns for athletic efficiency and performance</li>
                                 <li className={classes.infoListItem}>Develop Power, Strength, and Speed for your Sport</li>
-                                <li className={classes.infoListItem}>Learn the proper movement patterns for athletic efficiency and performance</li>
-                                <li className={classes.infoListItem}>Transfer strength and speed training to the field, court, mats, wherever you compete</li>
+                                <li className={classes.infoListItem}>Explore the boundaries of your athleticism and express your full abilities</li>
                                 <li className={classes.infoListItem}>Develop confidence and self-reliance in a competitive environment</li>
                                 <li className={classes.infoListItem}>Become battle-tested in your off-seasons to be ready for the main event</li>
                             </ul>
@@ -169,9 +165,34 @@ export default function Home() {
                         height: "875px"
                     }}
                 >
-                    <h1 className={classes.sectionHeader}>
-                        Get Involved
-                    </h1>
+                    <h1 className={classes.sectionHeader}>What Do the People Think of GSP?</h1>
+
+                    <p className={classes.bodyText}>
+                        <em>
+                            Great place to workout. My son is a football player who loves to workout here. Great people and awesome motivators. 
+                            He has helped my son transform his body for football. Highly recommend this place. Shane is the best!
+                        </em> - Jill Barnes
+                    </p>
+
+                    <p className={classes.bodyText}>
+                        <em>
+                            You will not be disappointed in this gym at all. All three of my kids are members here and they absolutely love it.
+                            My oldest son is now a freshmen in college on the Wrestling team lifting more then most on his team. GSP is 100% the reason for this. The BEST, go check them out.
+                        </em> - Levies
+                    </p>
+
+                    <p className={classes.bodyText}>
+                        <em>
+                            I&#8217;ve been to so many gyms and none compare to Garden State Performance. Every day is different and challenging and the results are awesome!
+                        </em> - Esther
+                    </p>
+
+                    <p className={classes.bodyText}>
+                        <em>
+                            GSP is welcoming place designed for all levels and ages. Work out at your own pace, increasing levels and timing each week. 
+                            Challenging while gaining endurance and strength
+                        </em> - OMT
+                    </p>
 
                     <p>  
                         <em>GSP's Principles of Athleticism </em> -
@@ -186,24 +207,10 @@ export default function Home() {
                         </a>
                     </p>
 
-                    <h1>
-                        How to Become a Member
-                    </h1>
-
-                    <ol className={classes.infoList}>
-                        <li className={classes.infoListItem}>Access the <a target="_blank"
-                            rel="noreferrer"
-                            href="https://forms.gle/nCCYsZVZ9rqEucBc9"
-                            className={classes.link}> link</a> below to receive the Principles of Athleticism article and schedule your FREE ATHLETIC ASSESSMENT</li>
-                        <li className={classes.infoListItem}>We will reach out to you within 24 hours to schedule your session</li>
-                        <li className={classes.infoListItem}>At your session we will conduct a free athletic assessment to guide your training </li>
-                        <li className={classes.infoListItem}> If we are a good fit for your training, we&#8217;ll get to work based on your assessment! It&#8217;s that simple. A program built for you to take your game to the next level </li>
-                    </ol>
-
                     <a 
                         target="_blank" 
                         rel="noreferrer" 
-                        href="https://forms.gle/nCCYsZVZ9rqEucBc9"
+                        href="https://forms.gle/26EjLbeDkoVDGhU78"
                         className={classes.link}
                     >
                         <h1>Become a Member <AddIcon /></h1>
