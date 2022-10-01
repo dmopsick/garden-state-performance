18b29dd160b34d289e8ca9fe2d961081acd1244f
Dan Mopsick <danmopsick@gmail.com>
2022-09-30 22:03:23 -0400

    #1 got the hours and programming page first iteration done

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -2,12 +2,17 @@
 export default function Hours() {
 
     return(
-        <div className='section pageContainer'>
+        <div className='section pageContainer redBackground'>
             <h1 className='sectionHeader'>Fall 2022 Hours</h1>
             <p className='sectionCaption'>These are the current hours unless otherwise posted on <a href="https://www.instagram.com/gardenstateperformance_/">Instagram</a>.</p>
             <ul className='infoList'>
+                <li className='infoListItem'>Sunday: Closed</li>
                 <li className='infoListItem'>Monday: 8 - 10 a.m. &amp; 5 - 7 p.m.</li>
-
+                <li className='infoListItem'>Tuesday: 9 - 10 a.m. &amp; 5 - 7 p.m.</li>
+                <li className='infoListItem'>Wednesday: 8 - 10 a.m. &amp; 5 - 7 p.m.</li>
+                <li className='infoListItem'>Thursday: 5 - 7 p.m.</li>
+                <li className='infoListItem'>Friday: 8 - 10 a.m. &amp; 4 - 6 p.m.</li>
+                <li className='infoListItem'>Saturday: Closed</li>
             </ul>
         </div>
 
--- src/components/Programming.js
+++ src/components/Programming.js
@@ -1,14 +1,29 @@
-import { makeStyles } from '@mui/styles';
-
-const useStyles = makeStyles(theme => ({
-
-}));
 
 export default function Programming() {
-    const classes = useStyles();
 
     return(
-        <div>Programming</div>
+        <div className='pageContainer redBackground'>
+            <div className='longSection'>
+                <h1 className='sectionHeader'>Our Programming</h1>
+                <h4 className='sectionSubheader'>Athletes</h4>
+                <p className='largeBodyText'>
+                    Our athlete programming is designed to create robust, well-rounded athletes ready to take on any challenger and whatever stressor their sport has to offer.
+                    The weight room is a phenomenal supplemental tool for any young athlete when used properly. Our programming focuses on filling in the gaps, suring up weak links, and working with imbalances that a hectic sports schedule may create on a young body. 
+                    We are proponents of mult-sport and multi-activity athletes, as this is the basis of athletic performance at a young age. GSP training sessions put the athlete first and work to create a strong organism ready for competitive battle wherever it may show up. 
+                </p>
+                <h4 className='sectionSubheader' 
+                    style={{
+                        marginTop: "20px"
+                    }}
+                >Adults</h4>
+                <p className='largeBodyText'>
+                    The human body is not designed to break down as we get older! Our lifestyles in the West create broken bodies too often, but this is not how it is meant to be. 
+                    With a potent blend of intensity, variety, movement, and strength, the Adult Fitness at Garden State Performance sets the foundation for a lifetime of movement, fulfillment, and freedom. 
+                    The greatest gift we have ever been given is our health. Take care of it with training that lets you explore your boundaries, develop new movement skills, and build a resiliency to life&apos;s challenges, both physically and mentally.
+                    Our Adult Programming welcomes any and all - the only requirement is a willingness to face challenges and get better day by day, brick by brick. 
+                </p>
+            </div>
+        </div>
 
     )
 
