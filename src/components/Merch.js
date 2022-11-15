6f36375eebf861ed887ca9bf40ce00cc8d698acf
Dan Mopsick <danmopsick@gmail.com>
2022-11-15 08:45:50 -0500

    Added captions for the modals and made the text nicer gonna show to shane and correct from there

--- src/components/Merch.js
+++ src/components/Merch.js
@@ -20,9 +20,7 @@ export default function Merch() {
 
 
     return(
-        <div className='longSection pageContainer redBackground' style={{
-            textAlign: 'center'
-        }}>
+        <div className='longSection pageContainer redBackground center-text'>
             <h1 className='sectionHeader'>Merch</h1>
             <p className='sectionCaption'>Represent GSP.</p>
             
@@ -31,34 +29,36 @@ export default function Merch() {
                 {viewerIsOpen ? (
                 <Modal onClose={closeLightbox}>
                     <Carousel
-                    currentIndex={currentImage}
-                    views={merchPhotos.map(x => ({
-                        ...x,
-                        srcset: x.srcSet,
-                        caption: x.title
-                    }))}
+                        currentIndex={currentImage}
+                        views={merchPhotos.map(x => ({
+                            ...x,
+                            srcset: x.srcSet,
+                            caption: x.title
+                        }))}
                     />
                 </Modal>
                 ) : null}
             </ModalGateway>
 
-            <h2>Member Pricing</h2>
+            <h2 className='sectionSubheader' style={{
+                marginTop: '25px'
+            }}>Member Pricing</h2>
             <ul className='infoList'>
                 <li className='infoListItem'>Cotton T Shirt - 1 for $25, 2 for $40</li>
                 <li className='infoListItem'>Tank Top - 1 for $25, 2 for $40</li>
                 <li className='infoListItem'>Dri-Fit - 1 for $40, 2 for $60</li>
             </ul>
 
-            <p>Member only offer - Dri-Fit + a T-Shirt/Tank Top for $50</p>
+            <p className='sectionCaption'>Member only offer - Dri-Fit + a T-Shirt/Tank Top for $50</p>
             
-            <h2>Non Member Pricing</h2>
+            <h2 className='sectionSubheader'>Non Member Pricing</h2>
             <ul className='infoList'>
                 <li className='infoListItem'>Cotton T Shirt - 1 for $30, 2 for $50</li>
                 <li className='infoListItem'>Tank Top - 1 for $30, 2 for $50</li>
                 <li className='infoListItem'>Dri-Fit - 1 for $45, 2 for $70</li>
             </ul>
 
-            <p>Cash, check, Venmo, CashApp, Zelle accepted by shirts. Shirts sold as long as they are in stock. Stop in or reach out to buy.</p>
+            <p className='sectionCaption'>Cash, check, Venmo, CashApp, Zelle accepted by shirts. Stop in or reach out to buy.</p>
             
         </div>
 
--- src/components/MerchPhotos.js
+++ src/components/MerchPhotos.js
@@ -4,27 +4,31 @@ export const merchPhotos = [
         width: 2,
         height: 2,
         alt: 'Spread of GSP shirts',
-        title: 'GSP Merch'
+        title: 'GSP Merch',
+        caption: 'GSP Merch'
     },
     {
         src: '/resources/images/merch/cotton_shirt.JPG',
         width: 2,
         height: 2,
         alt: 'GSP T-Shirt',
-        title: 'GSP T-Shirt'
+        title: 'GSP T-Shirt',
+        caption: 'GSP T-Shirt'
     },
     {
         src: '/resources/images/merch/women_tank_top.JPG',
         width: 2,
         height: 2,
         alt: 'GSP tank top',
-        title: 'GSP Tank Top'
+        title: 'GSP Tank Top',
+        caption: 'GSP Tank Top'
     },
     {
         src: '/resources/images/merch/dri_fit_shirt.JPG',
         width: 2,
         height: 2,
         alt: 'GSP Dri-Fit shirt',
-        title: 'GSP Dri-Fit shirt'
+        title: 'GSP Dri-Fit shirt',
+        caption: 'GSP Dri-Fit Shirt'
     },
 ];
