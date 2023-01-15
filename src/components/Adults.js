9ab9af0bf2077f3729ece291372f3c9d0798f273
Dan Mopsick <danmopsick@gmail.com>
2023-01-15 15:23:22 -0500

    Winter2023 working on the adults page

---
+++ src/components/Adults.js
@@ -0,0 +1,21 @@
+import AddIcon from '@mui/icons-material/Add';
+
+export default function Adults() {
+    return (
+        <div className='pageContainer redBackground'>
+            <div className='paddingBottom'>
+                <p className='largeBodyText'>
+                    Being a healthy, fully realized adult is simple.
+                    <br/><br/>
+                    Eat right. Exercise. Socialize with loved ones. Keep learning. I mean, that&#8217;s like four things, how hard can it really be?
+                    <br/><br/>
+                    The truth is most folks struggle mightily with the one task that should be everyone&#8217;s top priority → <em>taking care of themselves</em>!
+                    <br/><br/>
+                </p>
+
+            </div>
+        </div>
+    
+    )
+
+}
