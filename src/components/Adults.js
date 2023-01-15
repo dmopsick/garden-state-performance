02839f5c8c01daa95f4cb6ea4843bc52a7ee853a
Dan Mopsick <danmopsick@gmail.com>
2023-01-15 15:29:04 -0500

    Winter2023 fleshing out the Adults page

--- src/components/Adults.js
+++ src/components/Adults.js
@@ -11,6 +11,16 @@ export default function Adults() {
                     <br/><br/>
                     The truth is most folks struggle mightily with the one task that should be everyone&#8217;s top priority → <em>taking care of themselves</em>!
                     <br/><br/>
+                    We are distracted by a million and one different agendas - the kids, the dog, our job, the car, the news, our coffee order was wrong, blah blah blah!
+                    <br/><br/>
+                    Add to these distractions the fact that once you do find some time to yourself for a meal or a workout, you are inundated with heavily processed foods, bullshit workouts, and a world of misinformation and salesmen peddling snake oils. 
+                    <br/><br/>
+                    “Well Shane, what the f*ck am I supposed to do?” … I&#8217;m glad you asked.
+                    <br/><br/>
+                    JUST GIVE UP!
+                    <br/><br/>
+                    ... I&#8217;m kidding.
+                    <br/><br/>
                 </p>
 
             </div>
